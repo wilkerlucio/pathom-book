647072ddff1a14498a0c940a0a21c3a4b53846f5
Wilker Lucio <wilkerlucio@gmail.com>
2018-02-07 00:17:16 -0200

    Pretty designs for mutation stars demo

--- src/com/wsscode/pathom/book/graphql/fulcro_network/github_mutation_stars.cljs
+++ src/com/wsscode/pathom/book/graphql/fulcro_network/github_mutation_stars.cljs
@@ -21,18 +21,25 @@
                    :github.repository/name
                    :github.repository/name-with-owner
                    :github.repository/viewer-has-starred]
-   :css           [[:.heart {:text-shadow "0 0 0 #2f2f2f"
-                             :color "transparent"
-                             :transition "text-shadow 300ms"
-                             :font-size "28px"}
+   :css           [[:div [:.button {:display "flex"
+                                    :align-items "center"
+                                    :margin "14px 0"}]]
+                   [:.heart {:text-shadow "0 0 0 #2f2f2f"
+                             :color       "transparent"
+                             :transition  "text-shadow 300ms"
+                             :font-size   "28px"
+                             :margin-bottom "-3px"
+                             :margin-right "14px"}
                     [:&.red {:text-shadow "0 0 0 #f50909"}]]]
    :css-include   []}
   (dom/div nil
-    (dom/div #js {:className (str (:heart css) " " (if viewer-has-starred (:red css)))} "❤️")
-    (if viewer-has-starred
-      (str "Great, thanks for the " name " love!")
-      (dom/button #js {:onClick #(fp/transact! this `[{(add-star {:input {:starrable-id ~id}}) [:client-mutation-id]}])}
-        (str "Give love to " name)))))
+    (dom/button #js {:onClick  #(fp/transact! this `[{(add-star {:input {:starrable-id ~id}}) [:client-mutation-id]}])
+                     :disabled viewer-has-starred
+                     :className (:button css)}
+      (dom/div #js {:className (str (:heart css) " " (if viewer-has-starred (:red css)))} "❤️")
+      (if viewer-has-starred
+        (dom/div nil "Great, thanks for the " (dom/strong nil name) " love!")
+        (dom/div nil "Give love (star) to " (dom/strong nil name))))))
 
 (def star-repo (fp/factory StarRepo {:keyfn :github.repository/name-with-owner}))
 
@@ -45,12 +52,14 @@
   {:initial-state (fn [_]
                     {::repos [#:github.repository{:name-with-owner "wilkerlucio/pathom"}
                               #:github.repository{:name-with-owner "fulcrologic/fulcro"}
-                              #:github.repository{:name-with-owner "fulcrologic/fulcro-inspect"}]})
+                              #:github.repository{:name-with-owner "fulcrologic/fulcro-css"}
+                              #:github.repository{:name-with-owner "fulcrologic/fulcro-inspect"}
+                              #:github.repository{:name-with-owner "fulcrologic/fulcro-spec"}]})
    :ident         (fn [] [::github-stars "singleton"])
    :query         [{::repos (fp/get-query StarRepo)}]
    :css-include   [StarRepo]}
   (dom/div nil
-    "Hello Star"
+    "Use the buttons bellow to send love to our favorite UI kit tools!"
     (if (-> repos first :github.repository/id)
       (map star-repo repos))))
 
@@ -60,7 +69,9 @@
     (fn [{:keys [reconciler]}]
       (fp/transact! reconciler [(list 'fulcro/load {:query   [{[:github.repository/owner-and-name ["wilkerlucio" "pathom"]] (fp/get-query StarRepo)}
                                                               {[:github.repository/owner-and-name ["fulcrologic" "fulcro"]] (fp/get-query StarRepo)}
-                                                              {[:github.repository/owner-and-name ["fulcrologic" "fulcro-inspect"]] (fp/get-query StarRepo)}]
+                                                              {[:github.repository/owner-and-name ["fulcrologic" "fulcro-css"]] (fp/get-query StarRepo)}
+                                                              {[:github.repository/owner-and-name ["fulcrologic" "fulcro-inspect"]] (fp/get-query StarRepo)}
+                                                              {[:github.repository/owner-and-name ["fulcrologic" "fulcro-spec"]] (fp/get-query StarRepo)}]
                                                     :refresh [::repos]})]))
 
     :networking
